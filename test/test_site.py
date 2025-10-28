61bedee83cde3b9b2a4d411da77ebbd6223fcdcb
ecoreee <e-tinkin@mail.ru>
2025-10-28 14:03:00 +0300

    Update test_site

--- test/test_site.py
+++ test/test_site.py
@@ -1,16 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 import pytest
 import time
 from pages.homepage import HomePage
 from pages.product import ProductPage
-
-
-@pytest.fixture
-def browser():
-    browser = webdriver.Chrome()
-    browser.maximize_window()
-    browser.implicitly_wait(3)
-    yield browser
 
 
 def test_open_s6(browser):
@@ -25,5 +19,7 @@ def test_two_monitors(browser):
     homepage = HomePage(browser)
     homepage.open()
     homepage.click_monitor()
-    time.sleep(2)  #TODO:
+    WebDriverWait(browser, 5).until(
+        expected_conditions.presence_of_all_elements_located((By.CSS_SELECTOR, '.card'))
+    )
     homepage.check_product_count(2)
