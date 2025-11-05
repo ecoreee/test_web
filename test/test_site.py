fa8137b3a3dc4d40b552477c34d8f85b019f72ac
ecoreee <e-tinkin@mail.ru>
2025-11-06 01:55:57 +0300

    Update test_site.py

--- test/test_site.py
+++ test/test_site.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import pytest
 import time
 from pages.homepage import HomePage
@@ -20,6 +21,6 @@ def test_two_monitors(browser):
     homepage.open()
     homepage.click_monitor()
     WebDriverWait(browser, 10).until(
-        expected_conditions.presence_of_all_elements_located((By.CSS_SELECTOR, '.card'))
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.card'))
     )
     homepage.check_product_count(2)
